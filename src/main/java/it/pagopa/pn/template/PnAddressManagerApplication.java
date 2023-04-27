6147c0601a192c9f6e236fbf3e4312fafbb8dd47
Simone Martelli <simone.martelli@emea.nttdata.com>
2023-04-27 16:45:41 +0200

    removed temp resource and added health check path

--- src/main/java/it/pagopa/pn/template/PnAddressManagerApplication.java
+++ src/main/java/it/pagopa/pn/template/PnAddressManagerApplication.java
@@ -4,12 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TemplateApplication {
-
+public class PnAddressManagerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TemplateApplication.class, args);
+        SpringApplication.run(PnAddressManagerApplication.class, args);
     }
-
 
 }
