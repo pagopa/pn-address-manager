c55f03e4f418f173bf48a373f5f5f911ba590784
mattdac <Matteo.dacunzo@emea.nttdata.com>
2023-05-08 15:07:31 +0200

    Added localstack endpoint

--- src/main/java/it/pagopa/pn/address/manager/config/EventBridgeConfiguration.java
+++ src/main/java/it/pagopa/pn/address/manager/config/EventBridgeConfiguration.java
@@ -1,7 +1,9 @@
 package it.pagopa.pn.address.manager.config;
 
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
-import com.amazonaws.services.eventbridge.*;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.eventbridge.AmazonEventBridgeAsync;
+import com.amazonaws.services.eventbridge.AmazonEventBridgeAsyncClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,14 +11,19 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class EventBridgeConfiguration {
 
+
     @Value("${aws.region}")
     private String awsRegion;
 
+
     @Bean
     public AmazonEventBridgeAsync amazonEventBridgeAsync() {
+        AwsClientBuilder.EndpointConfiguration endpointConfiguration =
+                new AwsClientBuilder.EndpointConfiguration("http://localhost:4566", awsRegion);
+
         return AmazonEventBridgeAsyncClientBuilder.standard()
                 .withCredentials(DefaultAWSCredentialsProviderChain.getInstance())
-                .withRegion(awsRegion)
+                .withEndpointConfiguration(endpointConfiguration)
                 .build();
     }
 
