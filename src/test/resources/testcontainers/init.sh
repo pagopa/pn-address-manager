a906914777e6ab0d89a38a5e29a88b67ea33987d
mattdac <Matteo.dacunzo@emea.nttdata.com>
2023-05-11 10:14:57 +0200

    Added Eventbrige scripts for localstack

--- src/test/resources/testcontainers/init.sh
+++ src/test/resources/testcontainers/init.sh
@@ -1,10 +1,33 @@
-echo "### CREATE QUEUES FIFO ###"
-queues_fifo="local-address-inputs.fifo"
-for qn in $(echo $queues_fifo | tr " " "\n"); do
-  echo creating queue fifo $qn ...
+echo "### CREATE QUEUES ###"
+queues="local-address-inputs"
+for qn in $(echo $queues | tr " " "\n"); do
+  echo creating queue $qn ...
   aws --profile default --region us-east-1 --endpoint-url http://localstack:4566 \
     sqs create-queue \
-    --attributes '{"DelaySeconds":"2","FifoQueue": "true","ContentBasedDeduplication": "true"}' \
+    --attributes '{"DelaySeconds":"2"}' \
     --queue-name $qn
 done
 
+echo "### CREATE EVENT BUS ###"
+event_bus_name="my-event-bus"
+aws --profile default --region us-east-1 --endpoint-url http://localstack:4566 \
+  events create-event-bus --name $event_bus_name
+
+echo "### CREATE RULE WITH CUSTOM PATTERN ###"
+rule_name="my-rule"
+pattern='{"source": ["pn-address-manager"], "detail-type": ["AddressManagerOutcomeEvent"]}'
+aws --profile default --region us-east-1 --endpoint-url http://localstack:4566 \
+  events put-rule --name $rule_name --event-pattern "$pattern" \
+  --event-bus-name $event_bus_name
+
+echo "### ENABLE RULE ###"
+aws --profile default --region us-east-1 --endpoint-url http://localstack:4566 \
+  events enable-rule --name $rule_name \
+  --event-bus-name $event_bus_name
+
+echo "### ADD TARGET TO RULE ###"
+target_arn="arn:aws:sqs:us-east-1:000000000000:local-address-inputs"
+aws --profile default --region us-east-1 --endpoint-url http://localstack:4566 \
+  events put-targets --rule $rule_name \
+  --targets "Id"="1","Arn"="$target_arn" \
+  --event-bus-name $event_bus_name
