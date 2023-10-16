177acec4f2eaa6e0c6cb8ab96ddecb394df0b625
flaminiaScarciofolo <flaminia.scarciofolo@nttdata.com>
2023-10-16 06:59:41 +0200

    fix sonarCloud lower bug

--- src/main/java/it/pagopa/pn/address/manager/exception/PnAddressManagerException.java
+++ src/main/java/it/pagopa/pn/address/manager/exception/PnAddressManagerException.java
@@ -9,7 +9,7 @@ public class PnAddressManagerException extends PnRuntimeException {
     private final String code;
 
     public PnAddressManagerException(String message, int status, String code){
-        super(message, null, status, null, null, null);
+        super(message, message, status, code, null, null);
         this.code = code;
     }
 
