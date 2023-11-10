d3feeeb52ae7ed7bb7d82964876fa7ca97ab2727
flaminiaScarciofolo <flaminia.scarciofolo@nttdata.com>
2023-11-10 18:14:49 +0100

    add InsecureHttpsCommonBaseClient for custom httpClient

--- src/main/java/it/pagopa/pn/address/manager/config/DeduplicaClientConfig.java
+++ src/main/java/it/pagopa/pn/address/manager/config/DeduplicaClientConfig.java
@@ -1,28 +1,17 @@
 package it.pagopa.pn.address.manager.config;
 
-import io.netty.handler.ssl.SslContext;
-import io.netty.handler.ssl.SslContextBuilder;
-import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
-import it.pagopa.pn.address.manager.exception.PnAddressManagerException;
 import it.pagopa.pn.address.manager.log.ResponseExchangeFilter;
 import it.pagopa.pn.address.manager.msclient.generated.postel.deduplica.v1.ApiClient;
 import it.pagopa.pn.address.manager.msclient.generated.postel.deduplica.v1.api.DeduplicaApi;
-
-import it.pagopa.pn.commons.pnclients.CommonBaseClient;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.netty.http.client.HttpClient;
-
-import javax.net.ssl.SSLException;
 
 @Configuration
 @RequiredArgsConstructor
 @Slf4j
-public class DeduplicaClientConfig extends CommonBaseClient {
+public class DeduplicaClientConfig extends InsecureHttpsCommonBaseClient {
 
     private final ResponseExchangeFilter responseExchangeFilter;
 
@@ -32,25 +21,5 @@ public class DeduplicaClientConfig extends CommonBaseClient {
                 .filters(f -> f.add(responseExchangeFilter))));
         apiClient.setBasePath(cfg.getDeduplicaBasePath());
         return new DeduplicaApi(apiClient);
-    }
-
-    @Override
-    protected WebClient.Builder enrichWithDefaultProps(WebClient.Builder builder) {
-        try {
-            SslContext sslContext = SslContextBuilder
-                    .forClient()
-                    .trustManager(InsecureTrustManagerFactory.INSTANCE)
-                    .build();
-
-            HttpClient httpClient = buildHttpClient().secure(t -> t.sslContext(sslContext));
-
-            return builder
-                    .filter(buildRetryExchangeFilterFunction())
-                    .clientConnector(new ReactorClientHttpConnector(httpClient));
-
-        } catch (SSLException e) {
-            log.error("Failed to initialize Deduplica client: {}", e.getMessage(), e);
-            throw new PnAddressManagerException("Failed to initialize Deduplica client", 500, "DEDUPLICA_CLIENT_ERROR");
-        }
     }
 }
--- src/main/java/it/pagopa/pn/address/manager/config/NormalizzatoreClientConfig.java
+++ src/main/java/it/pagopa/pn/address/manager/config/NormalizzatoreClientConfig.java
@@ -1,29 +1,19 @@
 package it.pagopa.pn.address.manager.config;
 
-import io.netty.handler.ssl.SslContext;
-import io.netty.handler.ssl.SslContextBuilder;
-import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
-import it.pagopa.pn.address.manager.exception.PnAddressManagerException;
 import it.pagopa.pn.address.manager.log.ResponseExchangeFilter;
 import it.pagopa.pn.address.manager.msclient.generated.postel.normalizzatore.v1.ApiClient;
 import it.pagopa.pn.address.manager.msclient.generated.postel.normalizzatore.v1.api.NormalizzatoreApi;
-import it.pagopa.pn.commons.pnclients.CommonBaseClient;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.netty.http.client.HttpClient;
-
-import javax.net.ssl.SSLException;
 
 @Configuration
 @RequiredArgsConstructor
 @Slf4j
-public class NormalizzatoreClientConfig extends CommonBaseClient {
+public class NormalizzatoreClientConfig extends InsecureHttpsCommonBaseClient {
 
     private final ResponseExchangeFilter responseExchangeFilter;
 
@@ -40,26 +30,4 @@ public class NormalizzatoreClientConfig extends CommonBaseClient {
     public void setReadTimeoutMillis(@Value("${pn.address-manager.normalizer-postel-read-timeout-millis}") int readTimeoutMillis) {
         super.setReadTimeoutMillis(readTimeoutMillis);
     }
-
-    @Override
-    protected WebClient.Builder enrichWithDefaultProps(WebClient.Builder builder) {
-        try {
-            SslContext sslContext = SslContextBuilder
-                    .forClient()
-                    .trustManager(InsecureTrustManagerFactory.INSTANCE)
-                    .build();
-
-
-            HttpClient httpClient = buildHttpClient().secure(t -> t.sslContext(sslContext));
-
-            return builder
-                    .filter(buildRetryExchangeFilterFunction())
-                    .clientConnector(new ReactorClientHttpConnector(httpClient));
-
-        } catch (SSLException e) {
-            log.error("Failed to initialize Deduplica client: {}", e.getMessage(), e);
-            throw new PnAddressManagerException("Failed to initialize Deduplica client", 500, "DEDUPLICA_CLIENT_ERROR");
-        }
-    }
-
 }
