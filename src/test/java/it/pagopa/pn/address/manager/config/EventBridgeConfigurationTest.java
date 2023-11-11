873c2ba47bcf10057870b93a49320d5157214448
vincenzo-racca-pa <vincenzo.racca@pagopa.it>
2023-11-11 16:09:05 +0100

    remove tests for sqs conf and dynamodb conf

--- src/test/java/it/pagopa/pn/address/manager/config/EventBridgeConfigurationTest.java
+++ src/test/java/it/pagopa/pn/address/manager/config/EventBridgeConfigurationTest.java
@@ -1,6 +1,7 @@
 package it.pagopa.pn.address.manager.config;
 
 import com.amazonaws.services.eventbridge.AmazonEventBridgeAsyncClient;
+import it.pagopa.pn.address.manager.config.springbootcfg.AwsConfigsActivation;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +10,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@ContextConfiguration(classes = {EventBridgeConfiguration.class})
+@ContextConfiguration(classes = {EventBridgeConfiguration.class, AwsConfigsActivation.class})
 @ExtendWith(SpringExtension.class)
 class EventBridgeConfigurationTest {
     @Autowired
