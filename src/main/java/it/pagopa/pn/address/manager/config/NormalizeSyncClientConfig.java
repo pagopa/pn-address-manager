2a0d7655849d57da6245de954083489f5d7a19a8
Eugenio Giuliana <eugenio.giuliana@nttdata.com>
2026-09-17 12:40:55 +0200

    [PN-21417] - Removed timeout

--- src/main/java/it/pagopa/pn/address/manager/config/NormalizeSyncClientConfig.java
+++ src/main/java/it/pagopa/pn/address/manager/config/NormalizeSyncClientConfig.java
@@ -4,8 +4,6 @@ import it.pagopa.pn.address.manager.generated.openapi.msclient.postel.sync.v1.Ap
 import it.pagopa.pn.address.manager.generated.openapi.msclient.postel.sync.v1.api.NormalizzatoreApi;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -21,11 +19,6 @@ public class NormalizeSyncClientConfig extends InsecureHttpsCommonBaseClient {
         return new NormalizzatoreApi(apiClient);
     }
 
-    @Autowired
-    @Override
-    public void setReadTimeoutMillis(@Value("${pn.address-manager.normalizer-postel-read-timeout-millis}") int readTimeoutMillis) {
-        super.setReadTimeoutMillis(readTimeoutMillis);
-    }
 }
 
 
